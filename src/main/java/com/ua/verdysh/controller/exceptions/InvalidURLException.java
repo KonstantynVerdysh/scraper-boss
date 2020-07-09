05d88bc34fc38efff1a06793baf90bb0f1bc570a
Verdysh <verdysh1991@gmail.com>
2020-07-09 17:21:42 +0300

    Remove unusing package exceptions

--- src/main/java/com/ua/verdysh/controller/exceptions/InvalidURLException.java
+++
@@ -1,8 +0,0 @@
-package com.ua.verdysh.controller.exceptions;
-
-public class InvalidURLException extends IllegalArgumentException {
-
-    public InvalidURLException(String s) {
-        super(s);
-    }
-}
